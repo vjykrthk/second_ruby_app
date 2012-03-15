6a0018159d84be20e33adb20ff398a5e9bb500fc
karthik <karthik@ubuntu.ubuntu-domain>
2012-03-15 12:17:36 +0530

    Second app initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecondApp::Application.config.secret_token = 'c30d49ce5b252a10063f6cfbba7b91ba554684c878390842e13c2ded7f2d5810a90285c199090460712a119350e517f2220f671ff6aed0d60492f7fbbc1a7069'
